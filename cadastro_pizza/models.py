f662f9fb495280e15bee0c573067907820890568
Bruno Viana <brunolviana22@hotmail.com>
2016-09-08 22:29:54 -0300

    Refatoração de views

--- cadastro_pizza/models.py
+++ cadastro_pizza/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Ingrediente(models.Model):
-    nome = models.CharField('nome do ingrediente', max_length=100, null=False, blank=False)
-    descricao = models.CharField('descrição do Ingrediente', max_length=255, null=False, blank=False)
+    nome = models.CharField('nome', max_length=100, null=False, blank=False)
+    descricao = models.CharField('descrição', max_length=255, null=False, blank=False)
 
     def __str__(self):
         return self.nome
@@ -16,12 +16,15 @@ class Ingrediente(models.Model):
         verbose_name_plural = 'Ingredientes'
 
 class Pizza(models.Model):
-    sabor = models.CharField('sabor da pizza', max_length=100, null=False, blank=False, unique=True)
-    descricao = models.CharField('descrição da pizza', max_length=255, null=False, blank=False)
-    ingredientes = models.ManyToManyField(Ingrediente, verbose_name='ingredientes da pizza', blank=True)
+    sabor = models.CharField('sabor', max_length=100, null=False, blank=False, unique=True)
+    descricao = models.CharField('descrição', max_length=255, null=False, blank=False)
+    ingredientes = models.ManyToManyField(Ingrediente, verbose_name='ingredientes', blank=True)
 
     def get_absolute_url(self):
         return reverse('pizzas:lista-pizzas')
+
+    def __str__(self):
+        return self.nome
 
     class Meta:
         db_table = 'pizzas'
