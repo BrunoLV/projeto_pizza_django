dc7e83fec2a546a34c8cd638fa70396a3be072da
Bruno Viana <brunolviana22@hotmail.com>
2016-09-13 00:40:06 -0300

    Colocando mysql

--- projeto_pizza_django/settings.py
+++ projeto_pizza_django/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'projeto_pizza_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pizzaria',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
